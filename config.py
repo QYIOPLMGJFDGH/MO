de1b0d4cf238999c720ca8451a691d82421c58e8
QYIOPLMGJFDGH <Uttamk1578@gmail.com>
2025-03-27 09:59:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-API_ID = 123456  # Telegram API ID
-API_HASH = "your_api_hash"  # Telegram API Hash
-BOT_TOKEN = "your_bot_token"  # Telegram Bot Token
-SESSION_STRING = "your_assistant_string_session"  # Assistant Client के लिए String Session
+API_ID = 16457832  # Telegram API ID
+API_HASH = "3030874d0befdb5d05597deacc3e83ab"  # Telegram API Hash
+BOT_TOKEN = "7252944407:AAGOWJeR_eWFQfY2hRrgDhJJ-POOZsqA6Go"  # Telegram Bot Token
+SESSION_STRING = "BQD7IGgAUDAQZ3ZIKBV77gw1fZZiFbjNwQq2tUHR-wAWjSkqo5f35Ihqu1Sv53Z-qfcWLKrMHJD_Z_UJDAgWfbJEA-xm1DZu6a6mJrd-z0yf-ouC4PIetHrzasZRIaVamwTmmdZ1-ghrMdH9459cFculJg2h3HuQhOS-KsfqUNxdi_l3NUcF5UWkOm_Og8FJw-MMqrKUo_JmTikxmeY3m95uTujjurGaulcZA93VvhTxlHXDp33RpnwxalowJrXb7mWNpvTgjnku0KJzy9aRG74W8W0WWoMI8-wEwlJiGM01-hlFpwrAvkb7sY5FUyo_uQU3LQuSidHQw3c4ADXuTvTnEwSeMwAAAAGvPSEfAA"  # Assistant Client के लिए String Session
