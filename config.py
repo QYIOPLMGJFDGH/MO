5108099be75fef8f6397b7722e146c28a591bb22
QYIOPLMGJFDGH <Uttamk1578@gmail.com>
2025-03-27 09:56:20 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+API_ID = 123456  # Telegram API ID
+API_HASH = "your_api_hash"  # Telegram API Hash
+BOT_TOKEN = "your_bot_token"  # Telegram Bot Token
+SESSION_STRING = "your_assistant_string_session"  # Assistant Client के लिए String Session
